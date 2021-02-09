3f2177b8c73f6168bcb560f55717e37e6e0db1f6
booleanhunter <ashwin01h@gmail.com>
2021-02-09 16:13:01 +0530

    ci(heroku): Add production server dependencies
    - Add gunicorn
    - Add django-heroku
    - Add Procfile (to explicitly declare what command should be executed to start your app)

--- kartpool/settings.py
+++ kartpool/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import environ
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,4 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+
+django_heroku.settings(locals())
